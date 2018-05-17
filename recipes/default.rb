d05af6ae9ea3ac763d5d0f7072e4cdd368289a55
Zak B. Elep <zakame@zakame.net>
2018-05-17 16:49:54 +0800

    fix CHEF-3694

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,13 +26,7 @@ end
 #################################################
 
 # Install additional Apache packages.
-packages = %w( libapache2-modsecurity libapache2-mod-spamhaus )
-
-packages.each do |p|
-  package 'install libapache2 package' do
-    package_name p
-  end
-end
+package %w( libapache2-modsecurity libapache2-mod-spamhaus )
 
 # Create the group that owns web content.
 group content_group do
